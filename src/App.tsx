ca7380a15d6176d93368f608e23894dc31d4fd74
Cat-Div7 <omarashraf11234@gmail.com>
2026-05-15 20:12:20 +0300

    style: add global variables for styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
+    <div className="flex min-h-screen items-center justify-center">
       <Button variant="outline">Button</Button>
-    </>
+    </div>
   );
 }
 
